60f8a9f81c2285a6784064e0727b932181a155a8
SamHDev <sam02h.huddart@gmail.com>
2019-02-22 01:01:01 +0000

    AAA

--- install/install.py
+++ install/install.py
@@ -1,1 +1,17 @@
 import subprocess
+
+text = """
+[Unit]
+Description=BlumeHome Daemon
+Wants=network-online.target
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/home/blume/
+
+StandardOutput=console
+
+[Install]
+WantedBy=multi-user.target
+"""
--- install/install.sh
+++ install/install.sh
@@ -1,1 +1,27 @@
-echo "Hello World!"
+#!/usr/bin/env bash
+echo "Blume Installer"
+echo "-------------------------"
+echo "Installing BlumeHome"
+
+echo "Installing Python"
+sudo apt-get install python3
+sudo apt-get install python3-pip
+
+echo "Installing Pip Libs"
+sudo python3 -m pip install requests
+sudo python3 -m pip install flask
+
+echo "Installing Bluetooth"
+sudo apt-get install bluetooth libbluetooth-dev
+sudo python3 -m pip install pybluez
+
+echo "Editing Hostname"
+sudo cp /etc/hostname /etc/hostname_backup
+echo "echo BlumeDevice >> /etc/hostname " | sudo bash
+sudo hciconfig hci0 name 'BlumeDevice'
+
+echo "Creating User"
+adduser --disabled-password --gecos "" blume
+passwd blume BlumeHome123!
+
+echo "Downloading Code"
